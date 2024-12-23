22100411aa0d8ac27df42bd674c7e0c8db66e972
Chathuraprasad0 <165113680+Chathuraprasad0@users.noreply.github.com>
2024-12-23 07:12:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
+SESSION_ID: 'HANSAMAL-MD=WEF3URRI#CvBCixTYCYVuuOjJQe5ybqJan30mewtrAyAsT7XpPpA',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94703537169' : process.env.OWNER_NUMBER
 };
